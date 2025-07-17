e003054fa2d8437b6d59c1b0f3210df7b75775b9
Harshita Jaiswal <hjaiswal.it@gmail.com>
2025-07-17 13:20:11 +0530

    Day5

--- Day5.py
+++ Day5.py
@@ -1,5 +1,46 @@
-i= 1
-while i<= 6:
-    print("Loops" , i)
-    i += 1
-print(i)
+# i= 1
+# while i<= 6:
+#     print("Loops" , i)
+#     i += 1
+# print(i)
+
+# A= 1
+# while A <=100 :
+#     print(A)
+#     A += 1
+
+# print("Loop Ended")
+
+# B = 100
+# while B>=1:
+#     print(B)
+#     B -=1
+
+# print("Loop Ended")
+
+
+# n= 3
+# while n/3 <= 10:
+#     print(n)
+#     n += 3
+
+# m =1 
+# no = int(input("Enternumber"))
+# while m<=10:
+#     print(no * m)
+#     m +=1 
+
+
+# z = 1
+# while z<= 10:
+#     print( z**2)
+#     z += 1
+
+# list = [1,4,9,16,25,36,49,64,81,100]
+
+# idx = 0
+# while idx<= len(list)-1:
+#     print(list[idx])
+#     idx +=1 
+
+
